ff54d41efabd8488524d9b2037708e94997a8e64
Roberto Cruz <Roberto.k.cruz@gmail.com>
2013-04-08 20:51:51 -0700

    hW 6

---
+++ HW6/default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HW6
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+
+        protected void Wizard1_FinishButtonClick(object sender, WizardNavigationEventArgs e)
+        {
+            LBLName.Text = TXBoxName.Text;
+            LBLCCNum.Text = Address.Text;
+            LBLCCType.Text = DDListCCType.Text;
+            LBLExpDate.Text = CExpiration.SelectedDate.ToString();
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
